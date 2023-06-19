f08d0ab243811c778d6bd4bbb367ae83028ec3ce
MyeongJunKim <myeongjun222@naver.com>
2023-06-19 14:51:37 +0900

    [230619] 색상 스토리 구조 수정

--- src/components/basic/Color/Color.tsx
+++ src/components/basic/Color/Color.tsx
@@ -1,6 +1,9 @@
 import { COLOR } from '@src/styles/color';
+import type { ColorCategory } from '@src/styles/color';
 
-import { Theme } from '@src/types/types';
+import Text from '@src/components/atoms/Text/Text';
+
+import type { Theme } from '@src/types/types';
 
 import classNames from 'classnames/bind';
 import style from './style.module.scss';
@@ -13,55 +16,90 @@ type Props = {
 function Color({ theme = 'light' }: Props) {
   return (
     <div className={cx('container', theme)}>
-      <div className={cx('title')}>{theme} Theme</div>
+      <Text typo='h2'>{theme} Theme</Text>
       <div className={cx('contents')}>
         {theme === 'light' &&
-          Object.keys(COLOR.light).map((color: string, idx: number) => {
-            const value = COLOR.light[color];
+          Object.keys(COLOR.light).map((category: string, idx: number) => {
+            const c = COLOR.light[category as ColorCategory];
             return (
-              <div className={cx('color')} key={`light-${idx}`}>
-                <div className={cx('name')}>{color}</div>
-                <div className={cx('value')}>{value}</div>
-                <div
-                  className={cx('preview')}
-                  style={{
-                    background: value,
-                  }}
-                ></div>
+              <div
+                className={cx('categories', theme)}
+                key={`${category}-${idx}`}
+              >
+                <Text typo='h3'>{category}</Text>
+                <div className={cx('color')}>
+                  {Object.keys(c).map((color: string, i: number) => {
+                    const value = c[color];
+                    return (
+                      <div className={cx('block')} key={`light-${i}`}>
+                        <Text typo='t2'>
+                          {category}-{color}
+                        </Text>
+                        <Text typo='s1'>{value}</Text>
+                        <div
+                          className={cx('preview', theme)}
+                          style={{
+                            background: value,
+                          }}
+                        ></div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             );
           })}
         {theme === 'dark' &&
-          Object.keys(COLOR.dark).map((color: string, idx: number) => {
-            const value = COLOR.dark[color];
+          Object.keys(COLOR.dark).map((category: string, idx: number) => {
+            const c = COLOR.dark[category as ColorCategory];
             return (
-              <div className={cx('color')} key={`dark-${idx}`}>
-                <div className={cx('name')}>{color}</div>
-                <div className={cx('value')}>{value}</div>
-                <div
-                  className={cx('preview')}
-                  style={{
-                    background: value,
-                  }}
-                ></div>
+              <div
+                className={cx('categories', theme)}
+                key={`${category}-${idx}`}
+              >
+                <Text typo='h3'>{category}</Text>
+                <div className={cx('color')}>
+                  {Object.keys(c).map((color: string, i: number) => {
+                    const value = c[color];
+                    return (
+                      <div className={cx('block')} key={`light-${i}`}>
+                        <Text typo='t2'>
+                          {category}-{color}
+                        </Text>
+                        <Text typo='s1'>{value}</Text>
+                        <div
+                          className={cx('preview', theme)}
+                          style={{
+                            background: value,
+                          }}
+                        ></div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             );
           })}
-        {Object.keys(COLOR.solid).map((color: string, idx: number) => {
-          const value = COLOR.solid[color];
-          return (
-            <div className={cx('color')} key={`solid-${idx}`}>
-              <div className={cx('name')}>{color}</div>
-              <div className={cx('value')}>{value}</div>
-              <div
-                className={cx('preview')}
-                style={{
-                  background: value,
-                }}
-              ></div>
-            </div>
-          );
-        })}
+        <div className={cx('categories', theme)}>
+          <Text typo='h3'>solid</Text>
+          <div className={cx('color')}>
+            {Object.keys(COLOR.solid).map((color: string, idx: number) => {
+              const value = COLOR.solid[color];
+              return (
+                <div className={cx('block')} key={`solid-${idx}`}>
+                  <Text typo='t2'>{color}</Text>
+                  <Text typo='s1'>{value}</Text>
+                  <div
+                    className={cx('preview', theme)}
+                    style={{
+                      background: value,
+                    }}
+                  ></div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
