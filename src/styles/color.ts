3838af28c01661f795909c6b8a78da605a7458e6
MyeongJunKim <myeongjun222@naver.com>
2023-06-19 14:51:08 +0900

    [230619] 색상 수정

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,81 +1,115 @@
 import color from './color.module.scss';
 
+type ColorCategory = 'primary' | 'blue' | 'green' | 'red';
+
 const COLOR: {
-  light: { [key: string]: string };
-  dark: { [key: string]: string };
+  light: {
+    primary: { [key: string]: string };
+    blue: { [key: string]: string };
+    green: { [key: string]: string };
+    red: { [key: string]: string };
+    background: { [key: string]: string };
+  };
+  dark: {
+    primary: { [key: string]: string };
+    blue: { [key: string]: string };
+    green: { [key: string]: string };
+    red: { [key: string]: string };
+    background: { [key: string]: string };
+  };
   solid: { [key: string]: string };
 } = {
   light: {
-    primary000: color.light_primary000,
-    primary100: color.light_primary100,
-    primary200: color.light_primary200,
-    primary300: color.light_primary300,
-    primary400: color.light_primary400,
-    primary500: color.light_primary500,
-    primary600: color.light_primary600,
-    primary700: color.light_primary700,
-    primary800: color.light_primary800,
-    primary900: color.light_primary900,
-    blue000: color.light_blue000,
-    blue100: color.light_blue100,
-    blue200: color.light_blue200,
-    blue300: color.light_blue300,
-    blue400: color.light_blue400,
-    blue500: color.light_blue500,
-    blue600: color.light_blue600,
-    blue700: color.light_blue700,
-    green000: color.light_green000,
-    green100: color.light_green100,
-    green200: color.light_green200,
-    green300: color.light_green300,
-    green400: color.light_green400,
-    green500: color.light_green500,
-    green600: color.light_green600,
-    green700: color.light_green700,
-    red000: color.light_red000,
-    red100: color.light_red100,
-    red200: color.light_red200,
-    red300: color.light_red300,
-    red400: color.light_red400,
-    red500: color.light_red500,
-    red600: color.light_red600,
-    red700: color.light_red700,
-    background: color.light_background,
+    primary: {
+      ['000']: color.light_primary000,
+      ['100']: color.light_primary100,
+      ['200']: color.light_primary200,
+      ['300']: color.light_primary300,
+      ['400']: color.light_primary400,
+      ['500']: color.light_primary500,
+      ['600']: color.light_primary600,
+      ['700']: color.light_primary700,
+      ['800']: color.light_primary800,
+      ['900']: color.light_primary900,
+    },
+    blue: {
+      ['000']: color.light_blue000,
+      ['100']: color.light_blue100,
+      ['200']: color.light_blue200,
+      ['300']: color.light_blue300,
+      ['400']: color.light_blue400,
+      ['500']: color.light_blue500,
+      ['600']: color.light_blue600,
+      ['700']: color.light_blue700,
+    },
+    green: {
+      ['000']: color.light_green000,
+      ['100']: color.light_green100,
+      ['200']: color.light_green200,
+      ['300']: color.light_green300,
+      ['400']: color.light_green400,
+      ['500']: color.light_green500,
+      ['600']: color.light_green600,
+      ['700']: color.light_green700,
+    },
+    red: {
+      ['000']: color.light_red000,
+      ['100']: color.light_red100,
+      ['200']: color.light_red200,
+      ['300']: color.light_red300,
+      ['400']: color.light_red400,
+      ['500']: color.light_red500,
+      ['600']: color.light_red600,
+      ['700']: color.light_red700,
+    },
+    background: {
+      global: color.light_background,
+    },
   },
   dark: {
-    primary000: color.dark_primary000,
-    primary100: color.dark_primary100,
-    primary200: color.dark_primary200,
-    primary300: color.dark_primary300,
-    primary400: color.dark_primary400,
-    primary500: color.dark_primary500,
-    primary600: color.dark_primary600,
-    primary700: color.dark_primary700,
-    blue000: color.dark_blue000,
-    blue100: color.dark_blue100,
-    blue200: color.dark_blue200,
-    blue300: color.dark_blue300,
-    blue400: color.dark_blue400,
-    blue500: color.dark_blue500,
-    blue600: color.dark_blue600,
-    blue700: color.dark_blue700,
-    green000: color.dark_green000,
-    green100: color.dark_green100,
-    green200: color.dark_green200,
-    green300: color.dark_green300,
-    green400: color.dark_green400,
-    green500: color.dark_green500,
-    green600: color.dark_green600,
-    green700: color.dark_green700,
-    red000: color.dark_red000,
-    red100: color.dark_red100,
-    red200: color.dark_red200,
-    red300: color.dark_red300,
-    red400: color.dark_red400,
-    red500: color.dark_red500,
-    red600: color.dark_red600,
-    red700: color.dark_red700,
-    background: color.dark_background,
+    primary: {
+      ['000']: color.dark_primary000,
+      ['100']: color.dark_primary100,
+      ['200']: color.dark_primary200,
+      ['300']: color.dark_primary300,
+      ['400']: color.dark_primary400,
+      ['500']: color.dark_primary500,
+      ['600']: color.dark_primary600,
+      ['700']: color.dark_primary700,
+    },
+    blue: {
+      ['000']: color.dark_blue000,
+      ['100']: color.dark_blue100,
+      ['200']: color.dark_blue200,
+      ['300']: color.dark_blue300,
+      ['400']: color.dark_blue400,
+      ['500']: color.dark_blue500,
+      ['600']: color.dark_blue600,
+      ['700']: color.dark_blue700,
+    },
+    green: {
+      ['000']: color.dark_green000,
+      ['100']: color.dark_green100,
+      ['200']: color.dark_green200,
+      ['300']: color.dark_green300,
+      ['400']: color.dark_green400,
+      ['500']: color.dark_green500,
+      ['600']: color.dark_green600,
+      ['700']: color.dark_green700,
+    },
+    red: {
+      ['000']: color.dark_red000,
+      ['100']: color.dark_red100,
+      ['200']: color.dark_red200,
+      ['300']: color.dark_red300,
+      ['400']: color.dark_red400,
+      ['500']: color.dark_red500,
+      ['600']: color.dark_red600,
+      ['700']: color.dark_red700,
+    },
+    background: {
+      global: color.dark_background,
+    },
   },
   solid: {
     white: color.white,
@@ -83,4 +117,5 @@ const COLOR: {
   },
 };
 
+export type { ColorCategory };
 export { COLOR };
