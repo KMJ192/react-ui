97084e1b624fe0f9dcf77f2759ade415fa3220e3
MyeongJunKim <myeongjun222@naver.com>
2023-06-21 00:06:38 +0900

    [230621] Radio 컴포넌트 추가

---
+++ src/components/atoms/Radio/Radio.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import type { OVER_RIDABLE_PROPS } from '@src/types/types';
+
+import classNames from 'classnames/bind';
+import style from './style.module.scss';
+const cx = classNames.bind(style);
+
+type BaseProps = {
+  children?: React.ReactNode;
+};
+
+const ELEMENT = 'div';
+
+type Props<T extends React.ElementType> = OVER_RIDABLE_PROPS<T, BaseProps>;
+
+function Radio<T extends React.ElementType = typeof ELEMENT>(
+  { children, className, ...props }: Props<T>,
+  ref: React.Ref<any>,
+) {
+  return (
+    <ELEMENT {...props} ref={ref} className={cx(className)}>
+      {children}
+    </ELEMENT>
+  );
+}
+
+export type RadioProps = Props<typeof ELEMENT>;
+export default React.forwardRef(Radio) as typeof Radio;
---
+++ src/stories/atoms/Radio.stories.tsx
@@ -0,0 +1,26 @@
+import type { StoryFn, Meta } from '@storybook/react';
+
+import Radio from '@src/components/atoms/Radio/Radio';
+import type { RadioProps } from '@src/components/atoms/Radio/Radio';
+
+const meta: Meta<typeof Radio> = {
+  title: 'UI/Atoms/Radio',
+  component: Radio,
+  parameters: {
+    componentSubtitle: 'Radio',
+  },
+  argTypes: {},
+};
+
+const Template = (args: RadioProps) => {
+  const { children, ...arg } = args;
+
+  return <Radio {...arg}>{children}</Radio>;
+};
+
+export const PrimaryRadio: StoryFn<RadioProps> = Template.bind({});
+PrimaryRadio.args = {
+  children: 'Radio',
+};
+
+export default meta;
