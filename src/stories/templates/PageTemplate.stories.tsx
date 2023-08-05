25818b3b8166d56e0c8f6e7bc36ef51744c358aa
MyeongJunKim <myeongjun222@naver.com>
2023-08-06 00:05:23 +0900

    feat: PageTemplate 스토리 생성

---
+++ src/stories/templates/PageTemplate.stories.tsx
@@ -0,0 +1,99 @@
+import type { StoryFn, Meta } from '@storybook/react';
+
+import PageTemplate from '@src/components/templates/PageTemplate/PageTemplate';
+import type { PageTemplateProps } from '@src/components/templates/PageTemplate/PageTemplate';
+import { useRef } from 'react';
+import { SideNavItem } from '@src/components/molecules/SideNav';
+
+const meta: Meta<typeof PageTemplate> = {
+  title: 'UI/Template/PageTemplate',
+  component: PageTemplate,
+  parameters: {
+    componentSubtitle: 'PageTemplate',
+  },
+  argTypes: {},
+};
+
+const Template = (args: PageTemplateProps) => {
+  const { children, ...arg } = args;
+
+  const navItem = useRef<Array<SideNavItem>>([
+    {
+      key: 'nav1',
+      contents: 'Nav1',
+      children: [
+        {
+          key: 'nav1-1',
+          contents: 'Nav1-1',
+          children: [
+            {
+              key: 'nav1-1-1',
+              contents: 'Nav1-1-1',
+            },
+            {
+              key: 'nav1-1-2',
+              contents: 'Nav1-1-2',
+            },
+          ],
+        },
+        {
+          key: 'nav1-2',
+          contents: 'Nav1-2',
+        },
+        {
+          key: 'nav1-3',
+          contents: 'Nav1-3',
+        },
+      ],
+    },
+    {
+      key: 'nav2',
+      contents: 'Nav2',
+      children: [
+        {
+          key: 'nav2-1',
+          contents: 'Nav2-1',
+        },
+        {
+          key: 'nav2-2',
+          contents: 'Nav2-2',
+        },
+      ],
+    },
+    {
+      key: 'nav3',
+      contents: 'Nav3',
+      children: [
+        {
+          key: 'nav3-1',
+          contents: 'Nav3-1',
+        },
+        {
+          key: 'nav3-2',
+          contents: 'Nav3-2',
+        },
+        {
+          key: 'nav3-3',
+          contents: 'Nav3-3',
+        },
+      ],
+    },
+  ]);
+
+  const onClickItem = (key: string | number) => {
+    console.log(key);
+  };
+
+  return (
+    <PageTemplate {...arg} menu={navItem.current} onClickItem={onClickItem}>
+      {children}
+    </PageTemplate>
+  );
+};
+
+export const TestStory: StoryFn<PageTemplateProps> = Template.bind({});
+TestStory.args = {
+  children: 'Test',
+};
+
+export default meta;
