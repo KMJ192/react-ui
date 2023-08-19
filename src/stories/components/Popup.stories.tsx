0f54633105975fe3bc3e6e44823ec441ef2c2e4a
MyeongJunKim <myeongjun222@naver.com>
2023-08-19 20:28:30 +0900

    feat: Popup컴포넌트 수정

---
+++ src/stories/components/Popup.stories.tsx
@@ -0,0 +1,38 @@
+import type { StoryFn, Meta } from '@storybook/react';
+
+import Popup from '@src/components/Popup/Popup';
+import type { PopupProps } from '@src/components/Popup/Popup';
+
+const meta: Meta<typeof Popup> = {
+  title: 'UI/Components/Popup',
+  component: Popup,
+  parameters: {
+    componentSubtitle: 'Popup',
+  },
+  argTypes: {
+    animation: {
+      options: ['fade', 'none'],
+      control: {
+        type: 'radio',
+      },
+    },
+  },
+};
+
+const Template = (args: PopupProps) => {
+  const { children, ...arg } = args;
+
+  return <Popup {...arg}>{children}</Popup>;
+};
+
+export const PrimaryPopup: StoryFn<PopupProps> = Template.bind({});
+PrimaryPopup.args = {
+  children: 'Popup',
+  animation: 'fade',
+  visible: true,
+  style: {
+    padding: '30px',
+  },
+};
+
+export default meta;
