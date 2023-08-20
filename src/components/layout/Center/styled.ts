a3d5c45e86a5d3da78043e7a89b855686f245d69
MyeongJunKim <myeongjun222@naver.com>
2023-08-20 11:47:12 +0900

    feat: layout 방향 Center 코드 추가

--- src/components/layout/Center/styled.ts
+++ src/components/layout/Center/styled.ts
@@ -5,29 +5,37 @@ import { css } from '@emotion/react';
 
 type Props = Pick<CenterProps, 'vertical' | 'horizontal'>;
 
+const verticalCenter = css`
+  align-items: center;
+  -webkit-box-align: center;
+`;
+
+const horizontalCenter = css`
+  justify-content: center;
+  -webkit-box-pack: center;
+`;
+
 const center = css`
   ${flex}
   height: 100%;
   width: 100%;
-  align-items: center;
-  -webkit-box-align: center;
-  justify-content: center;
-  -webkit-box-pack: center;
+  ${verticalCenter}
+  ${horizontalCenter}
 `;
 
 const Container = styled.div<Props>`
   ${flex}
   height: 100%;
   width: 100%;
-  align-items: ${(props) => props.vertical && 'center'};
-  -webkit-box-align: ${(props) => props.vertical && 'center'};
-  justify-content: ${(props) => props.horizontal && 'center'};
-  -webkit-box-pack: ${(props) => props.horizontal && 'center'};
+  align-items: ${({ vertical }) => vertical && 'center'};
+  -webkit-box-align: ${({ vertical }) => vertical && 'center'};
+  justify-content: ${({ horizontal }) => horizontal && 'center'};
+  -webkit-box-pack: ${({ horizontal }) => horizontal && 'center'};
 `;
 
 const Styled = {
   Container,
 };
 
-export { center };
+export { center, verticalCenter, horizontalCenter };
 export default Styled;
