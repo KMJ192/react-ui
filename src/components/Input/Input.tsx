af9ea5d984fc9bcc15db8e0584355df62e7ffd0b
MyeongJunKim <myeongjun222@naver.com>
2023-09-07 23:01:11 +0900

    feat: Input 컴포넌트 기능 축소

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
-import { When } from '@ssamssam/react-modules';
-
-import Flex from '@src/layout/Flex/Flex';
-
-import type { OVER_RIDABLE_PROPS, SIZE } from '@src/types/types';
+import type { OVER_RIDABLE_PROPS } from '@src/types/types';
 
 import classNames from 'classnames/bind';
 import style from './style.module.scss';
 const cx = classNames.bind(style);
 
 type BaseProps = {
-  size?: SIZE;
   error?: boolean;
-  leftIcon?: React.ReactNode;
-  rightIcon?: React.ReactNode;
 };
 
 const DEFAULT_ELEMENT = 'input';
@@ -22,40 +15,17 @@ const DEFAULT_ELEMENT = 'input';
 type Props<T extends React.ElementType> = OVER_RIDABLE_PROPS<T, BaseProps>;
 
 function Input<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
-  {
-    as,
-    size = 'md',
-    leftIcon,
-    rightIcon,
-    error,
-    className,
-    ...props
-  }: Props<T>,
+  { as, error, className, ...props }: Props<T>,
   ref: React.Ref<any>,
 ) {
   const ELEMENT = as || DEFAULT_ELEMENT;
 
   return (
-    <Flex className={cx('container')}>
-      <When condition={leftIcon !== undefined}>
-        <div className={cx('icon', 'left', size)}>{leftIcon}</div>
-      </When>
-      <ELEMENT
-        {...props}
-        ref={ref}
-        className={cx(
-          'input',
-          size,
-          leftIcon !== undefined && 'left-icon',
-          rightIcon !== undefined && 'right-icon',
-          { error },
-          className,
-        )}
-      ></ELEMENT>
-      <When condition={rightIcon !== undefined}>
-        <div className={cx('icon', 'right', size)}>{rightIcon}</div>
-      </When>
-    </Flex>
+    <ELEMENT
+      {...props}
+      ref={ref}
+      className={cx('input', { error }, className)}
+    ></ELEMENT>
   );
 }
 
