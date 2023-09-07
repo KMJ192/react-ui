19b2d5241fe2d5e8c8b50c76099ecfdbf6ee3948
MyeongJunKim <myeongjun222@naver.com>
2023-09-07 17:08:28 +0900

    feat: Switch 컴포넌트 기능 개선

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -4,8 +4,6 @@ import Flex from '@src/layout/Flex/Flex';
 
 import type { OVER_RIDABLE_PROPS } from '@src/types/types';
 
-import { getStyle } from './calcStyle';
-
 import classNames from 'classnames/bind';
 import style from './style.module.scss';
 const cx = classNames.bind(style);
@@ -14,9 +12,6 @@ type BaseProps = {
   children?: React.ReactNode;
   checked?: boolean;
   disabled?: boolean;
-  width?: number;
-  height?: number;
-  bulletSize?: number;
 };
 
 const DEFAULT_ELEMENT = 'div';
@@ -31,40 +26,29 @@ function Switch<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
     disabled = false,
     size = 'md',
     className,
-    width,
-    height,
-    bulletSize,
     ...props
   }: Props<T>,
   ref: React.Ref<any>,
 ) {
   const ELEMENT = as || DEFAULT_ELEMENT;
 
-  const { switchStyle, bulletStyle, fontStyle } = getStyle({
-    width,
-    height,
-    bulletSize,
-  });
-
   return (
     <Flex
       {...props}
       as={ELEMENT as any}
       ref={ref}
-      className={cx('switch', size, { checked }, { disabled }, className)}
+      className={cx(
+        'switch',
+        size,
+        { checked },
+        { disabled },
+        children !== undefined && 'is-children',
+        className,
+      )}
     >
-      <div
-        className={cx('switch-body', { checked }, { disabled })}
-        style={switchStyle}
-      ></div>
-      <div
-        className={cx('switch-bullet', { checked }, { disabled })}
-        style={bulletStyle}
-      ></div>
-      <span
-        className={cx('children', { checked }, { disabled })}
-        style={fontStyle}
-      >
+      <div className={cx('switch-body', { checked }, { disabled })}></div>
+      <div className={cx('switch-bullet', { checked }, { disabled })}></div>
+      <span className={cx('children', { checked }, { disabled })}>
         {children}
       </span>
     </Flex>
