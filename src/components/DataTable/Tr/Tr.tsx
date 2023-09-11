0ec35a2d78a8a1c2fece2a12e0059cfdeb9f1759
MyeongJunKim <myeongjun222@naver.com>
2023-09-12 00:52:55 +0900

    feat: DataTable 코드 개선

--- src/components/DataTable/Tr/Tr.tsx
+++ src/components/DataTable/Tr/Tr.tsx
@@ -16,7 +16,7 @@ const DEFAULT_ELEMENT = 'tr';
 type Props<T extends React.ElementType> = OVER_RIDABLE_PROPS<T, BaseProps>;
 
 function Tr<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
-  { as, children, disabled, className, ...props }: Props<T>,
+  { as, children, disabled = false, className, ...props }: Props<T>,
   ref: React.Ref<any>,
 ) {
   const ELEMENT = as || DEFAULT_ELEMENT;
