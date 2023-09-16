39eed8e49327ccabc1ef076ba829948fd241bc4f
MyeongJunKim <myeongjun222@naver.com>
2023-09-17 01:44:55 +0900

    feat: Tab hooks 코드 개선

--- src/components/Tab/hooks/useSelectTab.tsx
+++ src/components/Tab/hooks/useSelectTab.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useCallback } from 'react';
+import { useRef, useEffect } from 'react';
 import { TabDirection, Offset, TabOption, Size } from '../types';
 
 type Props = {
@@ -13,35 +13,35 @@ function useSelectTab({ selected, options, direction }: Props) {
   const optionsRef = useRef<HTMLDivElement>(null);
   const tabLineRef = useRef<HTMLDivElement>(null);
 
-  const getSizeInfo: () => Array<TabLinePosition> = useCallback(() => {
-    if (optionsRef.current) {
-      const options = optionsRef.current;
-      const info = [];
-      const len = options.children.length;
-      let left = 0;
-      let top = 0;
-      for (let i = 0; i < len; i++) {
-        const option = options.children[i];
-        const { width, height } = option.getBoundingClientRect();
-        info[i] = {
-          width,
-          height,
-          left,
-          top,
-        };
-        left += width;
-        top += height;
-      }
-
-      return info;
-    }
-    return [];
-  }, []);
-
   useEffect(() => {
     if (selected < 0 || options.length - 1 < selected) {
       return;
     }
+    const getSizeInfo = (): Array<TabLinePosition> => {
+      if (optionsRef.current) {
+        const options = optionsRef.current;
+        const info = [];
+        const len = options.children.length;
+        let left = 0;
+        let top = 0;
+        for (let i = 0; i < len; i++) {
+          const option = options.children[i];
+          const { width, height } = option.getBoundingClientRect();
+          info[i] = {
+            width,
+            height,
+            left,
+            top,
+          };
+          left += width;
+          top += height;
+        }
+
+        return info;
+      }
+      return [];
+    };
+
     const sizeInfo = getSizeInfo();
 
     if (sizeInfo.length > 0 && tabLineRef.current) {
