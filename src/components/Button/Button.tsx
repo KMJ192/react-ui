91c1b93a6e0768d8045e8aa9a3c6ebfcde64fb6d
MyeongJunKim <myeongjun222@naver.com>
2023-11-22 22:44:11 +0900

    코드 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -51,14 +51,7 @@ function Button<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
       {...props}
       ref={ref}
       disabled={disabled || loading}
-      className={cx(
-        'button',
-        variant,
-        shape,
-        className,
-        { disabled },
-        { loading },
-      )}
+      className={cx('button', variant, shape, className, { loading })}
     >
       <Center className={cx('contents')}>
         {leftIcon && <Flex className={cx('icon')}>{leftIcon}</Flex>}
